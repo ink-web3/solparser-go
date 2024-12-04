9fcaa61ac67273039bd7beffd52eeb1743d8704b
umi <9@umi.cat>
2024-12-04 17:58:54 +0800

    INIT

---
+++ types/event.go
@@ -0,0 +1,15 @@
+package types
+
+type TokenAmt struct {
+	Code   string `json:"code"`
+	Amount string `json:"amount"`
+}
+
+type SwapTransactionEvent struct {
+	Sender          string   `json:"sender"`
+	Receiver        string   `json:"receiver"`
+	InToken         TokenAmt `json:"inToken"`
+	OutToken        TokenAmt `json:"outToken"`
+	PoolAddress     string   `json:"poolAddress"`
+	MarketProgramId string   `json:"market"`
+}
